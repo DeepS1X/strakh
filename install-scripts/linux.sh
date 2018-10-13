53830f525ef5be63027a724c29bcc578f0373dd2
root <root@localhost.localdomain>
2018-10-13 10:02:35 +0100

    Added all the information new people need to not fail.

--- install-scripts/linux.sh
+++ install-scripts/linux.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-apt = $(find /usr/bin/apt)
-
-if [ "$apt" = "/usr/bin/apt" ]; then
+if [ -a "/usr/bin/apt" ]; then
+  apt update
+  apt upgrade
   apt install iptables-persistent
   apt install aide
   apt install lynis
   apt install tcpdump
+  apt install fail2ban
+  apt install apparmor
+  apt install clamav
+  apt install rkhunter
+  apt install chkrootkit
+  apt install acct
 fi
